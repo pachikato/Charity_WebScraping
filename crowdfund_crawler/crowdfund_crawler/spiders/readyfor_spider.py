f647f62016c84e6b14111f554f7ba12eef1d5191
Hiroki Kato <katooo1205@gmail.com>
2018-09-07 22:23:07 +0900

    Readyfor Charityの1ページ目にあるプロジェクトのタイトルを辞書型で取得できるようにした

---
+++ crowdfund_crawler/crowdfund_crawler/spiders/readyfor_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class ReadyforSpider(scrapy.Spider):
+    name="readyfor_spider"
+    start_urls = ['https://readyfor.jp/tags/charity']
+    def parse(self,response):
+        for url in response.xpath("//a[name(..)='article']/@href").extract():
+            yield scrapy.Request(response.urljoin(url), self.parse_project)
+
+    def parse_project(self,response):
+        for project_name in response.xpath("//h1[name(..)='div'][../@class='Project-visual__title']/a/text()").extract():
+            yield {'project_name' : project_name}
