ce0656aab5d022e15dea18f4dd40648e93867244
Hiroki Kato <katooo1205@gmail.com>
2018-09-08 02:05:54 +0900

    ページの横断化

--- crowdfund_crawler/crowdfund_crawler/spiders/readyfor_spider.py
+++ crowdfund_crawler/crowdfund_crawler/spiders/readyfor_spider.py
@@ -3,9 +3,13 @@ import scrapy
 class ReadyforSpider(scrapy.Spider):
     name="readyfor_spider"
     start_urls = ['https://readyfor.jp/tags/charity']
+
     def parse(self,response):
         for url in response.xpath("//a[name(..)='article']/@href").extract():
             yield scrapy.Request(response.urljoin(url), self.parse_project)
+
+        for nextpage in response.xpath("//a[name(..)='span'][../@class='next']/@href").extract():
+            yield response.follow(nextpage,self.parse)
 
     def parse_project(self,response):
         for project_name in response.xpath("//h1[name(..)='div'][../@class='Project-visual__title']/a/text()").extract():
