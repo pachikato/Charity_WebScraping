b13ef51c183ccaabb2cc3e45c447cb0984a705cb
akihito_oka <a_oka@DM95876.local>
2018-09-09 03:15:12 +0900

    campfireの必要項目を全て取得できるようにした、DB挿入は未実装

--- crowdfund_crawler/crowdfund_crawler/spiders/campfire_spider.py
+++ crowdfund_crawler/crowdfund_crawler/spiders/campfire_spider.py
@@ -1,18 +1,18 @@
 import scrapy
 from crowdfund_crawler.items import DonationProject, DonationLog, DonationLoader
+from datetime import datetime
 
 
 class CampfireSpider(scrapy.Spider):
     name = 'campfire_spider'
     allowed_domains = ['camp-fire.jp']
-    # エンドポイント（クローリングを開始するURLを記載する）
     start_urls = [
         'http://camp-fire.jp/projects/category/social-good/popular'
     ]
-
     custom_settings = {
         "DOWNLOAD_DELAY": 1,
     }
+    current_date = datetime.now().strftime("%Y-%m-%d")
 
     def parse(self, response):
         self.logger.info('start parsing.')
@@ -30,28 +30,49 @@ class CampfireSpider(scrapy.Spider):
     def parse_projects(self, response):
         self.logger.info('Start project page parsing.')
 
-        # プロジェクトごとの項目を抜き出す
-        log_loader = DonationLoader(item=DonationLog(), response=response)
+        # プロジェクト名
+        project_name = response.xpath("//div[@class='header_top']/h2[@class='header_top__title']/text()").extract_first()
 
-        
-        # プロジェクトごとの項目を抜き出す
+        # 募集方式
+        system = response.xpath("//div[@class='project_status']/div[@class='status']/p/strong/text()|//section[@class='all-in']/text()").re_first("All-In|All-or-Nothing")
+
+        # 募集期日
+        end_date = response.xpath("//div[@class='project_status']/div[@class='status']/p/strong/text()|//section[@class='all-in']/strong/text()").re_first(r'\d{4}/\d{1,2}/\d{1,2}').replace('/', '-')
+
+        # カテゴリ
+        category = response.xpath("//section[@class='title']//a[contains(@href,'/projects/category/')]/text()|//div[@class='header_top']//a[contains(@href,'/projects/category/')]/text()").extract_first()
+
+        # 寄付金額ごとの項目を取り出す
         for return_section in response.xpath("//aside[@id='return__section']"):
             project_loader = DonationLoader(item=DonationProject(), response=response)
-            
-            # プロジェクト名
-            project_loader.add_xpath('project_name', "//div[@class='header_top']/h2[@class='header_top__title']/text()")
+            log_loader = DonationLoader(item=DonationLog(), response=response)
 
             # 寄付金額
             donation_unit_price = return_section.xpath(".//div[@class='return__price']/span/text()").extract_first()
 
             # リターン
             return_list = return_section.xpath(".//p[@class='return__list readmore']/text()").extract()
-            
+
+            # パトロン
+            patron = return_section.xpath(".//div[@class='return__info']/text()").re_first(r'(?<=パトロン：)(.*)(?=人)')
+
+            # 募集期間で変化しない項目をloaderに追加
+            project_loader.add_value('project_name', project_name)
+            project_loader.add_value('system', system)
+            project_loader.add_value('end_date', end_date)
+            project_loader.add_value('category', category)
             project_loader.add_value('donation_unit_price', donation_unit_price)
-            project_loader.add_value('return_list', return_list) 
-            
-            yield project_loader.load_item()
-            
-        # yield project_loader.load_item()
+            project_loader.add_value('return_list', return_list)
+            project_loader.add_value('source', 'campfire')
+            project_loader.add_value('created_at', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+            # 時系列で変化する項目をloaderに追加
+            log_loader.add_value('access_date', self.current_date)
+            log_loader.add_value('project_name', project_name)
+            log_loader.add_value('donation_unit_price', donation_unit_price)
+            log_loader.add_value('patron', patron)
+
+            #yield project_loader.load_item()
+            yield log_loader.load_item()
 
         self.logger.info('Complete project page parsing.')
